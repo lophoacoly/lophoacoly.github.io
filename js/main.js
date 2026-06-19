27789f6b7da094e8ccd7af1e0c1129009d721839
lophoacoly <lophoacoly@gmail.com>
2026-06-19 10:48:24 +0700

    update bangvangvinhdanh

--- js/main.js
+++ js/main.js
@@ -104,6 +104,17 @@
             }
         }
     });
+    // Vinh danh học viên - Magnific Popup
+    $(document).ready(function() {
+    $('.image-popup-vertical-fit').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+        verticalFit: true
+        }
+    });
+    });
     
 })(jQuery);
 
